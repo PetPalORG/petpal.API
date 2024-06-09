353f4977879c10f9ea5b593e6f599e4a4860d698
paolomv02 <u202010039@upc.edu.pe>
2024-06-09 12:11:45 -0500

    fix: delete vs files

--- PetPalBack/obj/Debug/net8.0/PetPalBack.AssemblyInfo.cs
+++ PetPalBack/obj/Debug/net8.0/PetPalBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetPalBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0228716dd7e45861d585b699dfa87241f04e9e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e150cc4f67239f4bddf57920171631075e98ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetPalBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetPalBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
