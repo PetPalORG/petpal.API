c21fcd86590e2e2255785ac09c8344207ed3b0a7
paolomv02 <u202010039@upc.edu.pe>
2024-06-25 04:37:20 -0500

    feat: shared

---
+++ PetPalBack/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetPalBack.Articles.Domain.Model.Aggregates;
+
+namespace PetPalBack.Shared.Infrastructure.Persistence.EFC.Configuration
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.AddCreatedUpdatedInterceptor();
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Article>().ToTable("articles");
+            builder.Entity<Article>().HasKey(a => a.id);
+            builder.Entity<Article>().Property(a => a.id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Article>().Property(a => a.title).IsRequired();
+            builder.Entity<Article>().Property(a => a.content).IsRequired();
+            builder.Entity<Article>().Property(a => a.author).IsRequired();
+            builder.Entity<Article>().Property(a => a.date).IsRequired();
+            builder.Entity<Article>().Property(a => a.imagePath).IsRequired();
+            builder.Entity<Article>().Property(a => a.authorImage).IsRequired();
+
+            //apply SnakeCase Naming Convention
+            builder.UseSnakeCaseWithPluralizedTableNamingConvention();
+
+        }
+    }
+}
