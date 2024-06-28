406e12041e7a53fd1f7c41a905a9d78c4a88c5d9
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-06-28 12:23:45 -0500

    update minorpatch swagger

--- PetPalBack/Program.cs
+++ PetPalBack/Program.cs
@@ -182,6 +182,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}else{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors("AllowAllPolicy");
